381c21afaaa977f066f586db9defff25e286d907
Danny Bruer <dbruer22@bsm7lgfwkair.bsm-online.local>
2019-12-03 11:19:19 -0600

    started the new file

---
+++ slow_approach.py
@@ -0,0 +1,22 @@
+import RoboPiLib as RPL
+import setup
+RPL.pinMode(7, RPL.INPUT)
+
+values = []
+
+#for i in range(0,100):
+ #  values.append(RPL.analogRead(7))
+  # basic = ((sum(values) / 100) * -1) + 100
+   #print basic
+
+x = 1
+sensor_pin = 7
+RPL.pinMode(sensor_pin,RPL.INPUT)
+while x == 1:
+    reading = RPL.analogRead(sensor_pin)
+    #RPL.servoWrite(1, )
+
+    if reading < 150:
+        RPL.servoWrite(1,1600)
+    elif reading >= 150:
+        RPL.servoWrite(1,0)
