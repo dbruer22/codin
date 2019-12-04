817af352b7e5be9b282e5a5f3f58e901beaa16aa
Danny Bruer <dbruer22@bsm7lgfwkair.bsm-online.local>
2019-12-04 12:43:08 -0600

    no more dig read print

--- slow_approach.py
+++ slow_approach.py
@@ -15,7 +15,7 @@ RPL.pinMode(sensor_pin,RPL.INPUT)
 while x == 1:
     an_reading = RPL.analogRead(sensor_pin)
     dig_reading = RPL.digitalRead(sensor_pin)
-    print dig_reading
+    #print dig_reading
     #RPL.servoWrite(1, )
     #print reading
     if dig_reading == 1:
