6a652770b3f7c36912f84946a297d19ad9db27bc
Danny Bruer <dbruer22@bsm7lgfwkair.bsm-online.local>
2019-12-04 13:22:19 -0600

    adding a change to slow down to help lower digits

--- slow_approach.py
+++ slow_approach.py
@@ -20,8 +20,12 @@ while x == 1:
     #RPL.servoWrite(1, )
     #print reading
     if dig_reading == 1:
-        RPL.servoWrite(1,1750 - (an_reading / 3))
-        RPL.servoWrite(0,1250 + (an_reading / 3))
+        if an_reading <= 280:
+            RPL.servoWrite(1,1750 - (an_reading / 2))
+            RPL.servoWrite(0,1250 + (an_reading / 2))
+        elif an_reading >= 280:
+            RPL.servoWrite(1,1750 - (an_reading / 3))
+            RPL.servoWrite(0,1250 + (an_reading / 3))
     elif dig_reading == 0:
         RPL.servoWrite(1,0)
         RPL.servoWrite(0,0)
