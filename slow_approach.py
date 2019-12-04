a66fd41e293cf0de30d8cafb251a042e74fc9cb8
Danny Bruer <dbruer22@bsm7lgfwkair.bsm-online.local>
2019-12-04 12:34:14 -0600

    fixed to equal to equal to

--- slow_approach.py
+++ slow_approach.py
@@ -17,10 +17,10 @@ while x == 1:
     dig_reading = RPL.digitalRead(sensor_pin)
     #RPL.servoWrite(1, )
     #print reading
-    if dig_reading = 1:
+    if dig_reading == 1:
         RPL.servoWrite(1,1750 - (an_reading / 3))
         RPL.servoWrite(0,1250 + (an_reading / 3))
-    elif dig_reading = 0:
+    elif dig_reading == 0:
         RPL.servoWrite(1,0)
         RPL.servoWrite(0,0)
 
