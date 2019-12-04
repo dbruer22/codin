2fcd4890e2b3b1704b9bee2e1150a7e008be67ab
Danny Bruer <dbruer22@bsm7lgfwkair.bsm-online.local>
2019-12-04 13:34:04 -0600

    remove the thing middle if statment

--- slow_approach.py
+++ slow_approach.py
@@ -20,13 +20,13 @@ while x == 1:
     #RPL.servoWrite(1, )
     #print reading
     if dig_reading == 1:
-        if an_reading <= 200:
+        if an_reading <= 250:
             RPL.servoWrite(1,1750 - (an_reading / 1))
             RPL.servoWrite(0,1250 + (an_reading / 1))
-        elif an_reading >= 200 <= 300:
-            RPL.servoWrite(1,1750 - (an_reading / 2))
-            RPL.servoWrite(0,1250 + (an_reading / 2))
-        elif an_reading >= 300:
+        #elif an_reading >= 200 <= 300:
+        #    RPL.servoWrite(1,1750 - (an_reading / 2))
+        #    RPL.servoWrite(0,1250 + (an_reading / 2))
+        elif an_reading >= 250:
             RPL.servoWrite(1,1750 - (an_reading / 3))
             RPL.servoWrite(0,1250 + (an_reading / 3))
     elif dig_reading == 0:
