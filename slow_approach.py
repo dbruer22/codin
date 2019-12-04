43c34828496a9a9e5c5cb4270a8824e1be6be734
Danny Bruer <dbruer22@bsm7lgfwkair.bsm-online.local>
2019-12-04 13:11:15 -0600

    revert

--- slow_approach.py
+++ slow_approach.py
@@ -15,22 +15,12 @@ RPL.pinMode(sensor_pin,RPL.INPUT)
 while x == 1:
     an_reading = RPL.analogRead(sensor_pin)
     dig_reading = RPL.digitalRead(sensor_pin)
-    #print dig_reading
-    #RPL.servoWrite(1, )
     print an_reading
+    #RPL.servoWrite(1, )
+    #print reading
     if dig_reading == 1:
-        if an_reading <= 170:
-            RPL.servoWrite(1,1600)
-            RPL.servoWrite(0,1400)
-        elif an_reading <= 200:
-            RPL.servoWrite(1,1550)
-            RPL.servoWrite(0,1450)
-        elif an_reading <= 250:
-            RPL.servoWrite(1,1525)
-            RPL.servoWrite(0,1475)
-        elif an_reading <= 300:
-            RPL.servoWrite(1,1510)
-            RPL.servoWrite(0,1490)
+        RPL.servoWrite(1,1750 - (an_reading / 1))
+        RPL.servoWrite(0,1250 + (an_reading / 1))
     elif dig_reading == 0:
         RPL.servoWrite(1,0)
         RPL.servoWrite(0,0)
