45a313c240da1f8265327710c06c3a0b8d9bb2f9
Danny Bruer <dbruer22@bsm7lgfwkair.bsm-online.local>
2019-12-04 13:18:26 -0600

    divder 3 back in

--- slow_approach.py
+++ slow_approach.py
@@ -20,8 +20,8 @@ while x == 1:
     #RPL.servoWrite(1, )
     #print reading
     if dig_reading == 1:
-        RPL.servoWrite(1,1750 - (an_reading / 1))
-        RPL.servoWrite(0,1250 + (an_reading / 1))
+        RPL.servoWrite(1,1750 - (an_reading / 3))
+        RPL.servoWrite(0,1250 + (an_reading / 3))
     elif dig_reading == 0:
         RPL.servoWrite(1,0)
         RPL.servoWrite(0,0)
