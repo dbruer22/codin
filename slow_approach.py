ab7bb97e19d6d714efdba9552c678abf24c73b56
Danny Bruer <dbruer22@bsm7lgfwkair.bsm-online.local>
2019-12-04 12:52:16 -0600

    changed to 1600 1300

--- slow_approach.py
+++ slow_approach.py
@@ -19,8 +19,8 @@ while x == 1:
     #RPL.servoWrite(1, )
     #print reading
     if dig_reading == 1:
-        RPL.servoWrite(1,1800 - (an_reading))
-        RPL.servoWrite(0,1200 + (an_reading))
+        RPL.servoWrite(1,1600 - (an_reading))
+        RPL.servoWrite(0,1300 + (an_reading))
     elif dig_reading == 0:
         RPL.servoWrite(1,0)
         RPL.servoWrite(0,0)
