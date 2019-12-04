59f506e1a39f856c9cd9a9e489aaeb12eb5714ba
Danny Bruer <dbruer22@bsm7lgfwkair.bsm-online.local>
2019-12-04 14:01:00 -0600

    time increased to 2

--- slow_approach.py
+++ slow_approach.py
@@ -30,7 +30,7 @@ while x == 1:
         RPL.servoWrite(1,1700 - ((an_reading / 3) - 55))
         RPL.servoWrite(0,1200 + ((an_reading / 3) + 55))
     elif dig_reading == 0:
-        time.sleep(1)
+        time.sleep(2)
         RPL.servoWrite(1,0)
         RPL.servoWrite(0,0)
 
