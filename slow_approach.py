ae6235622d6670ab80fc71240bb1ef93743b8ed0
Danny Bruer <dbruer22@bsm7lgfwkair.bsm-online.local>
2019-12-06 11:10:24 -0600

    changed the an imput pin

--- slow_approach.py
+++ slow_approach.py
@@ -3,10 +3,10 @@ import setup
 import time
 x = 1
 sensor_pin = 16
-sensorpin = 7
+an_sensorpin = 1
 RPL.pinMode(sensor_pin,RPL.INPUT)
 while x == 1:
-    an_reading = RPL.analogRead(sensorpin)
+    an_reading = RPL.analogRead(an_sensorpin)
     dig_reading = RPL.digitalRead(sensor_pin)
     if dig_reading == 1:
         RPL.servoWrite(1,1700 - ((an_reading / 3) - 55))
