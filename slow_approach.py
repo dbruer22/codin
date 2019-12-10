883f0d429a414ef03dcb331122cca7a9419530aa
Danny Bruer <dbruer22@bsm7lgfwkair.bsm-online.local>
2019-12-10 11:12:29 -0600

    adding wallavoid

--- slow_approach.py
+++ slow_approach.py
@@ -12,6 +12,6 @@ while x == 1:
         RPL.servoWrite(1,1700 - ((an_reading / 3) - 55))
         RPL.servoWrite(0,1200 + ((an_reading / 3) + 55))
     elif dig_reading == 0:
-        time.sleep(3)
+        time.sleep(2.3)
         RPL.servoWrite(1,0)
         RPL.servoWrite(0,0)
--- wall_avoid.py
+++ wall_avoid.py
@@ -1,17 +1,62 @@
 import RoboPiLib as RPL
 import setup
 import time
-x = 1
-sensor_pin = 16
-sensorpin = 7
-RPL.pinMode(sensor_pin,RPL.INPUT)
-while x == 1:
+def wall_avoid_varibles():
+    x = 1
+    sensor_pin = 16
+    sensorpin = 7
+    RPL.pinMode(sensor_pin,RPL.INPUT)
     an_reading = RPL.analogRead(sensorpin)
-    dig_reading = RPL.digitalRead(sensor_pin)
-    if dig_reading == 1:
+    bow_read = RPL.digitalRead(sensor_pin)
+    starboard_read = RPL.digitalRead(15)
+    port_read = RPL.digitalRead(14)
+    port_side = 0
+    starboard_side = 1
+wall_avoid_varibles()
+
+def port_move():
+    RPL.servoWrite(starboard_side,1450)
+    RPL.servoWrite(port_side,1550)
+def starboard_move():
+    RPL.servoWrite(starboard_side,1550)
+    RPL.servoWrite(port_side,1450)
+def stern_move():
+    RPL.servoWrite(starboard_side,1550)
+    RPL.servoWrite(port_side,1450)
+def bow_move():
+    RPL.servoWrite(1,1700 - ((an_reading / 3) - 55))
+    RPL.servoWrite(0,1200 + ((an_reading / 3) + 55))
+def conditions():
+    if bow_read == 0 and starboard_read == 0 and port_read == 1:
+        port_move()
+    elif bow_read == 1 and starboard_read == 0 and port_read == 1:
+        port_move()
+    elif bow_read == 0 and starboard_read == 1 and port_read == 0:
+        starboard_move()
+    elif bow_read == 1 and starboard_read == 1 and port_read == 0:
+        starboard_move()
+    elif bow_read == 0 and starboard_read == 1 and port_read == 1:
+        bow_move()
+    elif bow_read == 0 and starboard_read == 0 and port_read == 0:
+        bow_move()
+    elif:
+
+    #elif bow_read == 0 and starboard_read == 1 and port_read == 0:
+    #    bow_move()
+    #elif bow_read == 0 and starboard_read == 0 and port_read == 1:
+    #    bow_move()
+
+def starboard_conditions():
+    if bow_read == 0 and starboard_read == 1 and port_read == 0:
+        starboard_move()
+    elif bow_read == 1 and starboard_read == 1 and port_read == 0:
+        starboard_move()
+
+while x == 1:
+    if bow_read or starboard_read or port_read == 1:
         RPL.servoWrite(1,1700 - ((an_reading / 3) - 55))
         RPL.servoWrite(0,1200 + ((an_reading / 3) + 55))
-    elif dig_reading == 0:
+    elif dig_reading or RPL.digitalRead(15) or RPL.digitalRead(14) == 0:
         if RPL.digitalRead(15) == 1:
             RPL.servoWrite(1,1450)
             RPL.servoWrite(0,1550)
