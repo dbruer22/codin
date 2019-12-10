84213f79edb3c28e01e9fafca29b11a763d31ad1
Danny Bruer <dbruer22@bsm7lgfwkair.bsm-online.local>
2019-12-10 11:15:12 -0600

    reeee

--- wall_avoid.py
+++ wall_avoid.py
@@ -52,20 +52,20 @@ def starboard_conditions():
     elif bow_read == 1 and starboard_read == 1 and port_read == 0:
         starboard_move()
 
-while x == 1:
-    if bow_read or starboard_read or port_read == 1:
-        RPL.servoWrite(1,1700 - ((an_reading / 3) - 55))
-        RPL.servoWrite(0,1200 + ((an_reading / 3) + 55))
-    elif dig_reading or RPL.digitalRead(15) or RPL.digitalRead(14) == 0:
-        if RPL.digitalRead(15) == 1:
-            RPL.servoWrite(1,1450)
-            RPL.servoWrite(0,1550)
-        elif RPL.digitalRead(15) == 0:
-            RPL.servoWrite(1,1470)
-            RPL.servoWrite(0,1530)
-        elif RPL.digitalRead(14) == 1:
-            RPL.servoWrite(1,1550)
-            RPL.servoWrite(0,1450)
-        elif RPL.digitalRead(14) == 0:
-            RPL.servoWrite(1,1550)
-            RPL.servoWrite(0,1450)
+#while x == 1:
+#    if bow_read or starboard_read or port_read == 1:
+#        RPL.servoWrite(1,1700 - ((an_reading / 3) - 55))
+#        RPL.servoWrite(0,1200 + ((an_reading / 3) + 55))
+#    elif dig_reading or RPL.digitalRead(15) or RPL.digitalRead(14) == 0:
+#        if RPL.digitalRead(15) == 1:
+#            RPL.servoWrite(1,1450)
+#            RPL.servoWrite(0,1550)
+#        elif RPL.digitalRead(15) == 0:
+#            RPL.servoWrite(1,1470)
+#            RPL.servoWrite(0,1530)
+#        elif RPL.digitalRead(14) == 1:
+#            RPL.servoWrite(1,1550)
+#            RPL.servoWrite(0,1450)
+#        elif RPL.digitalRead(14) == 0:
+#            RPL.servoWrite(1,1550)
+#            RPL.servoWrite(0,1450)
