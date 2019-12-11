922e8cfca50327a238de5e5c0ad730a5163d2215
Danny Bruer <dbruer22@bsm7lgfwkair.bsm-online.local>
2019-12-11 13:46:24 -0600

    added numbers after all ands

--- wall_avoid_V2.py
+++ wall_avoid_V2.py
@@ -5,16 +5,16 @@ bowan_read = RPL.analogRead(7)
 starboardan_read = RPL.analogRead(6)
 
 def conditions():
-    if portan_read and starboardan_read <= 350:
+    if portan_read <= 350 and starboardan_read <= 350:
         RPL.servoWrite(1,1600)
         RPL.servoWrite(0,1400)
-    elif portan_read and starboardan_read >= 350:
+    elif portan_read >= 350 and starboardan_read >= 350:
         RPL.servoWrite(1,1600)
         RPL.servoWrite(0,1400)
-    elif portan_read >= 350 and starboardan_read < 350:
+    elif portan_read >= 350 and starboardan_read <= 349:
         RPL.servoWrite(1,1600)
         RPL.servoWrite(0,1550)
-    elif starboardan_read >= 350 and portan_read < 350:
+    elif starboardan_read >= 350 and portan_read <= 349:
         RPL.servoWrite(1,1400)
         RPL.servoWrite(0,1400)
     print portan_read
