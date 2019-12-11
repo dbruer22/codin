38544ac145a66a4368634161709408cdf6e56f34
Danny Bruer <dbruer22@bsm7lgfwkair.bsm-online.local>
2019-12-11 14:06:13 -0600

    makes it a while true loop so it can avoid on the fly

--- wall_avoid_V2.py
+++ wall_avoid_V2.py
@@ -5,18 +5,20 @@ bowan_read = RPL.analogRead(7)
 starboardan_read = RPL.analogRead(6)
 
 def conditions():
-    if portan_read <= 350 and starboardan_read <= 350:
-        RPL.servoWrite(1,1400)
-        RPL.servoWrite(0,1600)
-    elif portan_read >= 350 and starboardan_read >= 350:
-        RPL.servoWrite(1,1600)
-        RPL.servoWrite(0,1400)
-    elif portan_read >= 350 and starboardan_read <= 349:
-        RPL.servoWrite(1,1450)
-        RPL.servoWrite(0,1450)
-    elif starboardan_read >= 350 and portan_read <= 349:
-        RPL.servoWrite(1,1550)
-        RPL.servoWrite(0,1550)
-    print portan_read
-    print starboardan_read
+    x = 1
+    while x == 1:
+        if portan_read <= 350 and starboardan_read <= 350:
+            RPL.servoWrite(1,1400)
+            RPL.servoWrite(0,1600)
+        elif portan_read >= 350 and starboardan_read >= 350:
+            RPL.servoWrite(1,1600)
+            RPL.servoWrite(0,1400)
+        elif portan_read >= 350 and starboardan_read <= 349:
+            RPL.servoWrite(1,1450)
+            RPL.servoWrite(0,1450)
+        elif starboardan_read >= 350 and portan_read <= 349:
+            RPL.servoWrite(1,1550)
+            RPL.servoWrite(0,1550)
+    #print portan_read
+    #print starboardan_read
 conditions()
