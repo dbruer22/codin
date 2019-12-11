0aef1c4a07e1ad183e9210f78bc04037e8f270ec
Danny Bruer <dbruer22@bsm7lgfwkair.bsm-online.local>
2019-12-11 13:39:11 -0600

    sadfkja;sldfji

--- wall_avoid_V2.py
+++ wall_avoid_V2.py
@@ -6,17 +6,17 @@ starboardan_read = RPL.analogRead(6)
 
 def conditions():
     if portan_read and starboardan_read <= 350:
-        RPL.servoWrite(1,1550)
-        RPL.servoWrite(0,1450)
+        RPL.servoWrite(1,1600)
+        RPL.servoWrite(0,1400)
     elif portan_read and starboardan_read >= 350:
-        RPL.servoWrite(1,1550)
-        RPL.servoWrite(0,1450)
+        RPL.servoWrite(1,1600)
+        RPL.servoWrite(0,1400)
     elif portan_read >= 350 and starboardan_read < 350:
-        RPL.servoWrite(1,1550)
+        RPL.servoWrite(1,1600)
         RPL.servoWrite(0,1550)
     elif starboardan_read >= 350 and portan_read < 350:
-        RPL.servoWrite(1,1450)
-        RPL.servoWrite(0,1450)
+        RPL.servoWrite(1,1400)
+        RPL.servoWrite(0,1400)
     print portan_read
     print starboardan_read
 conditions()
