fc8b436d645040474d77a0bfb7ecacc6907e0574
Danny Bruer <dbruer22@bsm7lgfwkair.bsm-online.local>
2019-12-11 14:03:41 -0600

    slows down turns

--- wall_avoid_V2.py
+++ wall_avoid_V2.py
@@ -12,11 +12,11 @@ def conditions():
         RPL.servoWrite(1,1600)
         RPL.servoWrite(0,1400)
     elif portan_read >= 350 and starboardan_read <= 349:
-        RPL.servoWrite(1,1400)
-        RPL.servoWrite(0,1400)
+        RPL.servoWrite(1,1450)
+        RPL.servoWrite(0,1450)
     elif starboardan_read >= 350 and portan_read <= 349:
-        RPL.servoWrite(1,1600)
-        RPL.servoWrite(0,1600)
+        RPL.servoWrite(1,1550)
+        RPL.servoWrite(0,1550)
     print portan_read
     print starboardan_read
 conditions()
