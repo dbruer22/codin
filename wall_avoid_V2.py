c226051a3ac72f3115489af7d99808f52efd90fb
Danny Bruer <dbruer22@bsm7lgfwkair.bsm-online.local>
2019-12-11 13:29:40 -0600

    new file wallavoid 2

---
+++ wall_avoid_V2.py
@@ -0,0 +1,20 @@
+import RoboPiLib as RPL
+import setup
+portan_read = RPL.analogRead(5)
+bowan_read = RPL.analogRead(7)
+starboardan_read = RPL.analogRead(6)
+
+def conditions():
+    if portan_read and starboardan_read <= 350:
+        RPL.servoWrite(1,1550)
+        RPL.servoWrite(0,1450)
+    elif portan_read and starboardan_read >= 350:
+        RPL.servoWrite(1,1550)
+        RPL.servoWrite(0,1450)
+    elif portan_read >= 350 and starboardan_read < 350:
+        RPL.servoWrite(1,1550)
+        RPL.servoWrite(0,1550)
+    elif starboardan_read >= 350 and portan_read < 350:
+        RPL.servoWrite(1,1450)
+        RPL.servoWrite(0,1450)
+conditions()
