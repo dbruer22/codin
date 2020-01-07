1ca5edef569b10dee8f4f3c04476b206538f13a6
Danny Bruer <dbruer22@bsm7lgfwkair.bsm-online.local>
2020-01-07 11:25:08 -0600

    fix it to go foward and turn

--- wall_avoid_V2.py
+++ wall_avoid_V2.py
@@ -5,20 +5,20 @@ bowan_read = RPL.analogRead(7)
 starboardan_read = RPL.analogRead(6)
 
 def port_turn():
-    RPL.servoWrite(0,1600)
     RPL.servoWrite(1,1600)
+    RPL.servoWrite(0,1600)
 
 def star_turn():
-    RPL.servoWrite(0,1400)
     RPL.servoWrite(1,1400)
+    RPL.servoWrite(0,1400)
 
 def bow_move():
-    RPL.servoWrite(0,1400)
-    RPL.servoWrite(1,1600)
+    RPL.servoWrite(1,1400)
+    RPL.servoWrite(0,1600)
 
 def stern_move():
-    RPL.servoWrite(0,1600)
-    RPL.servoWrite(1,1400)
+    RPL.servoWrite(1,1600)
+    RPL.servoWrite(0,1400)
 
 def conditions():
     x = 1
@@ -27,13 +27,13 @@ def conditions():
         bowan_read = RPL.analogRead(7)
         starboardan_read = RPL.analogRead(6)
         if portan_read <= 350 and starboardan_read <= 350:
-            stern_move()
-        elif portan_read >= 350 and starboardan_read >= 350:
             bow_move()
+        elif portan_read >= 350 and starboardan_read >= 350:
+            stern_move()
         elif portan_read >= 350 and starboardan_read <= 349:
-            star_turn()
-        elif starboardan_read >= 350 and portan_read <= 349:
             port_turn()
+        elif starboardan_read >= 350 and portan_read <= 349:
+            star_turn()
         #print portan_read
         #print starboardan_read
 conditions()
