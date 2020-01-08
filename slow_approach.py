c38187b172b543296857c8f78b54a65049857a73
Danny Bruer <dbruer22@bsm7lgfwkair.bsm-online.local>
2020-01-08 13:08:17 -0600

    switched stuff roung

--- slow_approach.py
+++ slow_approach.py
@@ -9,10 +9,10 @@ while x == 1:
     an_reading = RPL.analogRead(7)
     dig_reading = RPL.digitalRead(sensor_pin)
     print dig_reading
-    if dig_reading == 1:
+    if dig_reading == 0:
         RPL.servoWrite(1,1700 - ((an_reading / 3) - 55))
         RPL.servoWrite(0,1200 + ((an_reading / 3) + 55))
-    elif dig_reading == 0:
+    elif dig_reading == 1:
         time.sleep(2.3)
         RPL.servoWrite(1,0)
         RPL.servoWrite(0,0)
