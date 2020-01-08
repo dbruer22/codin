dc1d609f12254962610e5ee507aead06fde6bc1c
Danny Bruer <dbruer22@bsm7lgfwkair.bsm-online.local>
2020-01-08 13:35:43 -0600

    rest mode to get ana to work better

--- slow_approach.py
+++ slow_approach.py
@@ -6,10 +6,12 @@ sensor_pin = 17
 an_sensorpin = 7
 RPL.pinMode(sensor_pin,RPL.INPUT)
 while x == 1:
+
     an_reading = RPL.analogRead(7)
     dig_reading = RPL.digitalRead(sensor_pin)
     print dig_reading
     if dig_reading == 1:
+        time.sleep(.3)
         RPL.servoWrite(1,1700 - ((an_reading / 3) - 55))
         RPL.servoWrite(0,1200 + ((an_reading / 3) + 55))
     elif dig_reading == 0:
