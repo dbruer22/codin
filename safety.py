cb2f942ac46d4fabe4d17b5a8f66afeb0f1f9cdc
DanB <dbruer22@bsmschool.org>
2020-01-31 11:42:04 -0600

    added safty

---
+++ safety.py
@@ -0,0 +1,14 @@
+import RoboPiLib_pwm as RPL
+import setup
+
+pul_pin = 4       #Your pin #
+dir_pin = 5       #Your pin #
+
+direction = 0     #can be 0 or 1
+speed = 50000      #smaller = faster
+
+RPL.pinMode(pul_pin,RPL.PWM)
+RPL.pinMode(dir_pin,RPL.OUTPUT)
+
+RPL.digitalWrite(dir_pin,direction) #sets the motor to forwards or backwards
+RPL.pwmWrite(pul_pin,speed,speed*2) #moves the motor
