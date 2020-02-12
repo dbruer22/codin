f969b0e709bf30a75ccfae08c99782722d818412
DanB <dbruer22@bsmschool.org>
2020-02-12 11:27:25 -0600

    this is to make it pulse contiuoulsy

--- runer.py
+++ runer.py
@@ -1,9 +1,13 @@
 import RoboPiLib as RPL
 import setup
-RPL.servoWrite(0,1400)
-RPL.servoWrite(1,1400)
-RPL.servoWrite(2,1400)
-RPL.servoWrite(3,1400)
-RPL.servoWrite(4,1400)
-RPL.servoWrite(5,1400)
-RPL.servoWrite(6,1400)
+x = 1
+if x = 1:
+    RPL.servoWrite(0,1400)
+    RPL.servoWrite(1,1400)
+    RPL.servoWrite(2,1400)
+    RPL.servoWrite(3,1400)
+    RPL.servoWrite(4,1400)
+    RPL.servoWrite(5,1400)
+    RPL.servoWrite(6,1400)
+elif:
+    RPL.servoWrite(0,1400)
