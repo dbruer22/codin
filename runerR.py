2a1ba0c14de81c13e736d799a0798693c1702e90
DanB <dbruer22@bsmschool.org>
2020-02-14 13:14:37 -0600

    this is a reverse runer command

---
+++ runerR.py
@@ -0,0 +1,11 @@
+import RoboPiLib as RPL
+import setup
+x = 1
+while x == 1:
+    RPL.servoWrite(0,1600)
+    RPL.servoWrite(1,1600)
+    RPL.servoWrite(2,1600)
+    RPL.servoWrite(3,1600)
+    RPL.servoWrite(4,1600)
+    RPL.servoWrite(5,1600)
+    RPL.servoWrite(6,1600)
