c3c63e62bf05d70a4ff79ba4d0fc88ca198e6a32
DanB <dbruer22@bsmschool.org>
2021-02-02 12:07:15 -0600

    this is some bs

--- camerarotate.py
+++ camerarotate.py
@@ -1,12 +1,12 @@
 import RoboPiLib as RPL
 import setup
-w = 'w'
-x = 0
-while x == 0:
-    while True:
-        usrin = input()
 
-        if usrin == 'w':
-            print 'you pressed w'
-        else:
-            print 'thats not a w'
+w = 'w'
+
+while True:
+    usrin = input()
+
+    if usrin == 'w':
+        print 'you pressed w'
+    else:
+        print 'thats not a w'
